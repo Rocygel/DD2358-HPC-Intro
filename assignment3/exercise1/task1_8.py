bae2f0ec37e6bca62d6cb88f2a3de694e8a32dca
Turb0M <eynar_eklof@yahoo.com>
2026-03-02 11:34:06 +0100

    Add Assignment III Task-1.8 to main.

---
+++ assignment3/exercise1/task1_8.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+from timeit import default_timer as timer
+from functools import wraps
+from gauss_seidel_numpy import gauss_seidel # Our cythonized function
+
+def main():
+    # instantiate 
+    N = [16, 32, 64, 128, 256] # grid sizes
+    rng = np.random.default_rng()
+    save_file = h5py.File("matricies.hdf5", "w")
+    
+    gs_iterations = 1000
+    run_iterations = 10
+    total_time = []
+
+    for n in N:
+        x = rng.random((n, n), dtype=np.float32)
+
+        # set grid values at boundaries to zero
+        for j in range(n):
+            x[j][0] = 0
+            x[0][j] = 0
+            x[j][n-1] = 0
+            x[n-1][j] = 0
+
+        t0 = timer()
+        for run in range (run_iterations):
+            for i in range(gs_iterations):
+                x = gauss_seidel(x)
+        t1 = timer()
+
+        total_time.append((t1 - t0) / run_iterations)
+        dset = save_file.create_dataset(f"matrix{n}", data=x)
+        dset.attrs['iterations'] = gs_iterations
+        dset.attrs['run_time'] = ((t1 - t0) / run_iterations) 
+
+    print(total_time)
+    plt.plot(N, total_time)
+    plt.title(f"Execution time for varying grid size ({gs_iterations} iterations)")
+    plt.xlabel("Grid lengths (N for N x N grid)")
+    plt.ylabel("Computation time (s)")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
